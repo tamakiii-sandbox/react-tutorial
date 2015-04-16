a350269717c4a1fd320b42fd288877380c2fcb24
tamakiii <tamaki.htm@gmail.com>
2015-04-17 01:09:00 +0900

    now get comments from dummy API(this doesn't work)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 var React = require('react');
 var CommentBox = require('./comment.jsx');
-var data = [
-  {author: "Pete Hunt", text: "(tutorial 13) This is one comment"},
-  {author: "Jordan Walke", text: "(tutorial 13) This is *another* comment"}
-];
-React.render(
-  <CommentBox data={data} />,
+
+React.renderComponent(
+  <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
