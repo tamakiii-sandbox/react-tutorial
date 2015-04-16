9a25a6a5669551ed0b86057faeb6bddc02156052
tamakiii <tamaki.htm@gmail.com>
2015-04-17 01:09:36 +0900

    bugfix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var CommentBox = require('./comment.jsx');
 
-React.renderComponent(
+React.render(
   <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
